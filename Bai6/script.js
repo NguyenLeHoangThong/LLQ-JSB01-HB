10042aaa6662f00aa8f3d6e206fac19118794976
thongnguyen <thongnguyen@pixelcent.com>
2023-07-17 08:39:00 +0700

    Upload buoi 6

---
+++ Bai6/script.js
@@ -0,0 +1,75 @@
+// number
+let age = 19
+let ageAfter4Years = age
+ageAfter4Years = 25
+let avg = 3.5
+
+// string
+let fullName = "Thong Nguyen"
+let fullName20 = fullName + " 20"
+
+
+// boolean: true / false
+let isUpGrade = false
+
+// null
+let avgMath = null
+
+// undefined
+let xe;
+
+
+
+
+// conditional
+const webAge = 0 
+
+// == là "ép kiểu" xong rồi so sánh. Ép kiểu nghĩa là biến từ kiểu dữ liệu này sang
+// kiểu dữ liệu khác
+
+// === là so sánh cả cái kiểu dữ liệu của 2 giá trị, nếu kiểu dữ liệu 2 giá trị
+// mà không giống nhau => False ngay!!!!
+
+
+// if (webAge === "0") { // number -> string
+//     console.log("True")
+// }
+// else {
+//     console.log("False")
+// }
+
+// let random = "Thong"; // false, "", 0, null, undefined ===> FALSE
+
+// if (random) {
+//     console.log("Random: true")
+// } else {
+//     console.log("Ngẫu nhiên: false")
+// }
+
+// WEAK TYPE
+let age1 = 16
+age1 = "Thong"
+
+
+// Tạo 1 biến tuổi = ...
+
+// nếu tuổi > 20 => tăng tuổi lên 1
+// nếu tuổi <= 20 và tuổi >= 16 => giảm tuổi 2
+// nếu tuổi < 16 => giảm tuổi 3
+
+// in tuổi sau khi chạy các lệnh ở
+// trên ra
+
+// let age2 = 25;
+
+// if (age2 > 20) {
+//     age2 = age2 + 1
+// }
+// else if (age2 >= 16 && age <= 20) {
+//     age2 = age2 -2
+// }
+// else {
+//     age2 = age2 - 3
+// }
+
+// console.log("age2 = ", age2)
